59377819eb29c0c0a0bb8cb053a9fbaa971b0e6c
Lesha Koss <regiusprod@gmail.com>
2016-11-25 22:09:17 +0600

    Add parse property for English locale

--- src/locale/en/index.js
+++ src/locale/en/index.js
@@ -1,7 +1,9 @@
 var buildDistanceInWordsLocale = require('./build_distance_in_words_locale/index.js')
 var buildFormatLocale = require('./build_format_locale/index.js')
+var buildParseLocale = require('./build_parse_locale/index.js')
 
 module.exports = {
   distanceInWords: buildDistanceInWordsLocale(),
-  format: buildFormatLocale()
+  format: buildFormatLocale(),
+  parse: buildParseLocale()
 }
